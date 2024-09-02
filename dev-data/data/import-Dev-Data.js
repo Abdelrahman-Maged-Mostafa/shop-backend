24fe70193945caf753be3ff3142ab9ac4c786378
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2024-09-02 20:28:05 +0300

    worked

--- dev-data/data/import-Dev-Data.js
+++ dev-data/data/import-Dev-Data.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const Tour = require('../../models/tourmodel');
-const Review = require('../../models/reviewModel');
+// const Tour = require('../../models/tourmodel');
+// const Review = require('../../models/reviewModel');
 const User = require('../../models/userModel');
 /////////////////////////////// run server
 dotenv.config({ path: './config.env' }); //should be before app require
@@ -17,15 +17,15 @@ mongoose
     console.log(connection.connections[0].base.connections[0].user);
   });
 //Read Json File
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+// const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
-const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
+// const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 const importData = async () => {
   try {
-    await Tour.create(tours);
-    await User.create(users, { validateBeforeSave: false });
-    await Review.create(reviews);
+    // await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: true });
+    // await Review.create(reviews);
     console.log('data loaded');
   } catch (err) {
     console.log(err);
@@ -35,9 +35,9 @@ const importData = async () => {
 
 const deletData = async () => {
   try {
-    await Tour.deleteMany();
+    // await Tour.deleteMany();
     await User.deleteMany();
-    await Review.deleteMany();
+    // await Review.deleteMany();
     console.log('data loaded');
   } catch (err) {
     console.log(err);
