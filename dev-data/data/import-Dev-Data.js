ee6b81a3a912e4bd5d03fa9c7667648dcf089704
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2024-09-02 23:44:56 +0300

    worked

--- dev-data/data/import-Dev-Data.js
+++ dev-data/data/import-Dev-Data.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const Item = require('../../models/itemmodel');
-// const Review = require('../../models/reviewModel');
+// const Item = require('../../models/itemmodel');
+const Review = require('../../models/reviewModel');
 // const User = require('../../models/userModel');
 /////////////////////////////// run server
 dotenv.config({ path: './config.env' }); //should be before app require
@@ -17,15 +17,15 @@ mongoose
     console.log(connection.connections[0].base.connections[0].user);
   });
 //Read Json File
-const items = JSON.parse(fs.readFileSync(`${__dirname}/items.json`, 'utf-8'));
+// const items = JSON.parse(fs.readFileSync(`${__dirname}/items.json`, 'utf-8'));
 // const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
-// const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 const importData = async () => {
   try {
-    await Item.create(items);
+    // await Item.create(items);
     // await User.create(users, { validateBeforeSave: true });
-    // await Review.create(reviews);
+    await Review.create(reviews);
     console.log('data loaded');
   } catch (err) {
     console.log(err);
@@ -35,9 +35,9 @@ const importData = async () => {
 
 const deletData = async () => {
   try {
-    await Item.deleteMany();
+    // await Item.deleteMany();
     // await User.deleteMany();
-    // await Review.deleteMany();
+    await Review.deleteMany();
     console.log('data loaded');
   } catch (err) {
     console.log(err);
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Tour = require('./itemmodel');
+const Item = require('./itemmodel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -11,20 +11,20 @@ const reviewSchema = new mongoose.Schema(
       ref: 'User',
       require: [true, 'Review must belong to a user'],
     },
-    tour: {
+    item: {
       type: mongoose.Schema.ObjectId,
-      ref: 'Tour',
-      require: [true, 'Review must belong to a tour'],
+      ref: 'Item',
+      require: [true, 'Review must belong to a item'],
     },
   },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } },
 );
 // we write this line of code to no one write over than 1 review in every tour that easy way to do it :)
-reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+reviewSchema.index({ item: 1, user: 1 }, { unique: true });
 
 reviewSchema.pre(/^find/, function (next) {
-  this.populate({ path: 'user', select: 'name photo' });
-  //   this.populate({ path: 'tour', select: 'name' });
+  this.populate({ path: 'user', select: 'name' });
+  //   this.populate({ path: 'item', select: 'name' });
   next();
 });
 // last thing in any folder after all meddle ware
@@ -35,12 +35,12 @@ reviewSchema.statics.calcAvrageRatings = async function (tourId) {
     { $group: { _id: '$tour', nRatings: { $sum: 1 }, avgRating: { $avg: '$rating' } } },
   ]);
   if (stats.length > 0) {
-    await Tour.findByIdAndUpdate(tourId, {
+    await Item.findByIdAndUpdate(tourId, {
       ratingsQuantity: stats[0].nRatings,
       ratingsAverage: stats[0].avgRating,
     });
   } else {
-    await Tour.findByIdAndUpdate(tourId, {
+    await Item.findByIdAndUpdate(tourId, {
       ratingsQuantity: 0,
       ratingsAverage: 4.5,
     });
@@ -48,7 +48,7 @@ reviewSchema.statics.calcAvrageRatings = async function (tourId) {
 };
 
 reviewSchema.post('save', function () {
-  this.constructor.calcAvrageRatings(this.tour);
+  this.constructor.calcAvrageRatings(this.item);
 });
 
 reviewSchema.pre(/^findOneAnd/, async function () {
@@ -56,7 +56,7 @@ reviewSchema.pre(/^findOneAnd/, async function () {
 });
 
 reviewSchema.post(/^findOneAnd/, async function () {
-  await this.r.constructor.calcAvrageRatings(this.r.tour);
+  await this.r.constructor.calcAvrageRatings(this.r.item);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
