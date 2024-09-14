48c80d9355d35c02caa2e5920778971f51ca288b
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2024-09-14 18:54:01 +0300

    worked

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,10 +4,18 @@ const catchAsync = require('../utils/catchAsync');
 exports.createNewOrder = catchAsync(async (req, res, next) => {
   req.body.user = req.user._id;
   if (req.body.status) delete req.body.status;
-  // const testTour = new Tour({ name: 'The Park Camper', price: 997 });
   const newOne = await Order.create(req.body);
   await res.status(201).json({
     status: 'success',
     data: { data: newOne },
+  });
+});
+
+exports.getAllOrders = catchAsync(async (req, res, next) => {
+  //   const orders = await Order.find({ user: req.user.id }).populate('user').exec();
+  const orders = await Order.find().populate('user').exec();
+  await res.status(201).json({
+    status: 'success',
+    data: { data: orders },
   });
 });
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { createNewOrder } = require('../controllers/orderController');
-const { protect } = require('../controllers/authController');
+const { createNewOrder, getAllOrders } = require('../controllers/orderController');
+const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.use(protect);
 
-router.route('/').get().post(createNewOrder);
+router.route('/').get(restrictTo('admin'), getAllOrders).post(createNewOrder);
 
 module.exports = router;
