23e8c62967f74105fe674077e71d0e848b2b78ad
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2024-09-14 18:11:12 +0300

    worked

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,6 +2,7 @@ const Order = require('../models/orderModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.createNewOrder = catchAsync(async (req, res, next) => {
+  req.body.user = req.user._id;
   if (req.body.status) delete req.body.status;
   // const testTour = new Tour({ name: 'The Park Camper', price: 997 });
   const newOne = await Order.create(req.body);
