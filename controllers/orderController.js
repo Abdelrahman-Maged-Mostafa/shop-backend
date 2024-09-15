8ec8c22520d6eb68b824c7f8489969f4fa41fba4
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2024-09-15 15:02:49 +0300

    worked

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,5 @@
 const Order = require('../models/orderModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.createNewOrder = catchAsync(async (req, res, next) => {
@@ -17,6 +18,27 @@ exports.getAllOrders = catchAsync(async (req, res, next) => {
   await res.status(201).json({
     status: 'success',
     data: { data: orders },
+  });
+});
+
+exports.getAllUserOrders = catchAsync(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user.id }).populate('user').exec();
+  await res.status(201).json({
+    status: 'success',
+    data: { data: orders },
+  });
+});
+
+exports.getUserOrder = catchAsync(async (req, res, next) => {
+  //   const orders = await Order.find({ user: req.user.id }).populate('user').exec();
+  const order = await Order.findById(req.params.id)
+    .populate('user') // Populate the user reference
+    .populate('items.item') // Populate the item reference within items
+    .exec();
+  if (order.user._id !== req.user.id) return new AppError('This is not your order', 403);
+  await res.status(201).json({
+    status: 'success',
+    data: { data: order },
   });
 });
 exports.getOrder = catchAsync(async (req, res, next) => {
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { createNewOrder, getAllOrders, getOrder } = require('../controllers/orderController');
+const {
+  createNewOrder,
+  getAllOrders,
+  getOrder,
+  getAllUserOrders,
+  getUserOrder,
+} = require('../controllers/orderController');
 const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
@@ -7,6 +13,8 @@ const router = express.Router();
 router.use(protect);
 
 router.route('/').get(restrictTo('admin'), getAllOrders).post(createNewOrder);
+router.route('/userOrder').get(getAllUserOrders);
+router.route('/userOrder/:id').get(getUserOrder);
 router.route('/:id').get(restrictTo('admin'), getOrder);
 
 module.exports = router;
