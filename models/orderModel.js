1af0f007002de121802f133fd34fd7d1232db565
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2024-09-21 11:26:13 +0300

    worked

--- models/orderModel.js
+++ models/orderModel.js
@@ -18,7 +18,7 @@ const OrderSchema = new mongoose.Schema({
   email: { type: String, required: true },
   address: { type: String, required: true },
   phone: { type: String, required: true },
-  transactionID: { type: String, required: true },
+  transactionID: { type: String },
   createdAt: { type: Date, default: Date.now() },
   items: [ItemSchema],
   user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
